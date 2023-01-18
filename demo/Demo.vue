ad1ac53ede899d7c86e8c8384de7051714f5e831
piotrzdziarski <piotr.m.zdziarski@protonmail.com>
2023-01-18 19:35:33 +0100

    Updated the demo.
    Now it is a standard 5 source demo—with an image, HTML video, YouTube video, Vimeo video, and an invalid source.

--- demo/Demo.vue
+++ demo/Demo.vue
@@ -1,45 +1,33 @@
 <template>
-    <div>
-        <button @click="openLightboxOnSlide(1)">Open on 1 slide</button>
-        <button @click="openLightboxOnSlide(2)">Open on 2 slide</button>
+	<div>
+		<button @click="toggler = !toggler">1</button>
 
-        <FsLightbox
-            :toggler="toggler"
-            :sources="[
-			'https://i.imgur.com/fsyrScY.jpg',
-			'https://www.youtube.com/watch?v=xshEZzpS4CQ',
-			'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
-		]"
-            :slide="slide"
-            :custom-attributes="[
-                {
-                    srcSet: 'demo/img/7.jpg 600w, demo/img/5.jpg 1200w',
-                    sizes: '(max-width: 600px) 600px, 1200px',
-                },
-                {
-                    poster: 'demo/img/7.jpg'
-                }
-            ]"
-        />
-    </div>
+		<FsLightbox
+			:toggler="toggler"
+			:sources="[
+				'1.jpg',
+				'https://www.youtube.com/watch?v=3nQNiWdeH2Q',
+				'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+				Vimeo,
+				'0'
+			]"
+		/>
+	</div>
 </template>
 
 <script>
 import FsLightbox from "../FsLightbox.vue";
+import Vimeo from "./Vimeo.vue";
 
 export default {
-    components: { FsLightbox },
-    data() {
-        return {
-            toggler: false,
-            slide: 1
-        }
-    },
-    methods: {
-        openLightboxOnSlide: function (number) {
-            this.slide = number;
-            this.toggler = !this.toggler;
-        }
-    }
+	components: { FsLightbox },
+	data() {
+		return {
+			toggler: false
+		}
+	},
+	created() {
+		this.Vimeo = Vimeo;
+	}
 }
 </script>
