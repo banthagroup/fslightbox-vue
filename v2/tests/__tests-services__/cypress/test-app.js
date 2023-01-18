6261c1b19ed8ba479498338e108d3513fb2c9e29
piotrzdziarski <piotr.m.zdziarski@protonmail.com>
2023-01-18 14:09:10 +0100

    Removed Cypress and integration testing.
    The changes to the vanilla JavaScript and React versions are being reflected.

--- v2/tests/__tests-services__/cypress/test-app.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-import TestComponents from "../../../../base/tests/__tests-services__/cypress/TestComponents.vue";
-
-new Vue({
-    el: "#app",
-    render: h => h(TestComponents)
-});
--- v3/tests/__tests-services__/cypress/test-app.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue';
-import TestComponents from "../../../../base/tests/__tests-services__/cypress/TestComponents.vue";
-
-const app = createApp(TestComponents);
-app.mount('#app');
