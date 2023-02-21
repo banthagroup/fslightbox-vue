817df26ce342433c36d4423f65720a07b6ffe956
piotrzdziarski <piotr.m.zdziarski@protonmail.com>
2023-02-21 18:42:55 +0100

    Adjusted the window resize actioner to the new stage system.

--- 1/c/sizes/setUpWindowResizeActioner.js
+++ 1/c/sizes/setUpWindowResizeActioner.js
@@ -1,38 +1,38 @@
-import { removeFromElementClassIfContains } from "../../h/elements/removeFromElementClassIfContains";
-import { TRANSFORM_TRANSITION_CLASS_NAME } from "../../cn/classes-names";
-
 export function setUpWindowResizeActioner(
     {
-        collections: { sourceMainWrapperTransformers, sourceSizers },
+        collections: { sourceSizers },
         core: { windowResizeActioner: self },
         data,
-        elements: { sourceMainWrappers },
-        props: { sources },
+        elements: { sources },
+	smw,
         stageIndexes
     }
 ) {
     self.runActions = () => {
-        // decreasing max source dimensions for better UX
         (innerWidth < 992) ?
             data.maxSourceWidth = innerWidth :
             data.maxSourceWidth = 0.9 * innerWidth;
         data.maxSourceHeight = 0.9 * innerHeight;
 
         for (let i = 0; i < sources.length; i++) {
-            removeFromElementClassIfContains(sourceMainWrappers[i], TRANSFORM_TRANSITION_CLASS_NAME);
-
-            if (i !== stageIndexes.current) {
-                sourceMainWrapperTransformers[i].negative();
-            }
+	    smw[i].d();
 
             /**
              * Invalid type doesn't have SourceSizer so need to check if it exists.
-             * React deletes refs after removing element so even if SourceSizer exists there might be no source, e.g.
-             * after reopening
+             * Sources are created 'tick' after main elements so after reopening lightbox there might be no source with
+             * created SourceSizer so there is also a need for checking if source exists.
              */
-            if (sourceSizers[i]) {
+            if (sourceSizers[i] && sources[i]) {
                 sourceSizers[i].adjustSize();
             }
         }
+
+	var pi=stageIndexes.previous,ni=stageIndexes.next;
+	if (pi !== undefined) {
+		smw[pi].ne();
+	}
+	if (ni !== undefined) {
+		smw[ni].p();
+	}
     };
 }
