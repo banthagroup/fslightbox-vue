1d3d6daee795845bf8fa3ab90143519daa4e66cc
piotrzdziarski <piotr.m.zdziarski@protonmail.com>
2023-02-24 16:28:25 +0100

    Squeezed the Vue 2 main demo file.

--- v2/d.js
+++ v2/d.js
@@ -1,7 +1,1 @@
-import Vue from "vue";
-import d from "../1/d.vue";
-
-new Vue({
-    el: "#a",
-    render: h => h(d)
-})
+import Vue from"vue";import d from"../1/d.vue";new Vue({el:"#a",render:h=>h(d)})
