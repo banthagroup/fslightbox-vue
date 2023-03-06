daa4cb5d0c7b3ae290a2e65017017c3559dcb849
piotrzdziarski <piotr.m.zdziarski@protonmail.com>
2023-03-06 08:53:30 +0100

    Fixed a path in the mounting function.

--- 1/c/m.js
+++ 1/c/m.js
@@ -1,1 +1,1 @@
-import{gsw}from"../../scrollbar/gsw";import{injectStylesIfNotInDom}from"../../styles/injectStylesIfNotInDom";export function m(o){var{data,props:{openOnMount}}=o;injectStylesIfNotInDom();data.scrollbarWidth=gsw();if(openOnMount){o.i()}}
+import{gsw}from"./scrollbar/gsw";import{injectStylesIfNotInDom}from"./styles/injectStylesIfNotInDom";export function m(o){var{data,props:{openOnMount}}=o;injectStylesIfNotInDom();data.scrollbarWidth=gsw();if(openOnMount){o.i()}}
